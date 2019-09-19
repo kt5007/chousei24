86f0083459a87642400270a198d13f447905ceb5
kt5007 <kenjin.0546@gmail.com>
2019-09-19 05:55:28 +0000

    push to login_logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  
 end
