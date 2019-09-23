5700e6359be18f40c8d99d8ef54113890a31066b
kt5007 <kenjin.0546@gmail.com>
2019-09-23 10:25:30 +0000

    20190923_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :email, presence: true
+    
+    VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX}
+    
+    VALID_PASS_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+    validates :password, format: { with: VALID_PASS_REGEX }
+    
+    validates :password_confirmation, presence: true
     
     has_secure_password
 end
