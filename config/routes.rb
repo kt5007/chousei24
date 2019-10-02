7feb6c1d7a1889d579419738097bf1614aee9560
kt5007 <kenjin.0546@gmail.com>
2019-10-02 07:13:44 +0000

    20191002_login_logoutブランチにプッシュ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
   
+  #get 'users/new'
+  root 'pages#help'
+  get 'pages/index'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
 end
